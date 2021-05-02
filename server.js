47811783f997beebe4a4bbdd2353b631333c6f79
Yash Garg <yash.garg@airtel.com>
2021-05-02 18:36:46 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 const mockUserData = [
     { name: 'Mark' },
@@ -22,6 +26,31 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+app.post('/login',function(req,res){
+	// Typically passwords are encrypted using something like bcrypt before sending to database
+	const username=req.body.username;
+	const password=req.body.password;
+
+	// This should come from the database
+	const mockUsername="yati";
+	const mockPassword="airtel@123";
+
+	if (username===mockUsername && password===mockPassword){
+		// In practice, use JSON web token sign method here to make an encrypted token
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
+
 })
 
 
